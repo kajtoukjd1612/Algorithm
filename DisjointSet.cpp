6f411f85631e164470dac0b397a84be6a80a6d50
Phong Pham <kajtoukjd1612@users.noreply.github.com>
2015-02-06 15:38:52 +0700

    Create DisjointSet.cpp

---
+++ DisjointSet.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int,int> ii;
+
+/* Disjoint Set */
+vector<int> Setname(1001),Setsize(1001);
+int NumSet;
+
+void InitSet(int n) {
+    NumSet = n;
+    Setsize.assign(n,1);
+    Setname.assign(n,0);
+    for(int i=0;i<n;++i) Setname[i] = i;
+}
+
+int Findset(int n) {
+    if(Setname[n] == n) return n;
+    else return Findset(Setname[n]);
+}
+
+bool Sameset(int i,int j) {
+    return Findset(i) == Findset(j);
+}
+
+void Union(int i,int j) {
+    if(!Sameset(i,j)) {
+        NumSet--;
+        Setsize[Findset(i)] += Setsize[Findset(j)];
+        Setname[Findset(i)] = Findset(j);
+    }
+}
+/* end */
