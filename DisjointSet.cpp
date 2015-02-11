166808031fadb7cf66451419eb001c7930310de8
Phong Pham <kajtoukjd1612@users.noreply.github.com>
2015-02-11 07:21:02 -0800

    Update DisjointSet.cpp

--- DisjointSet.cpp
+++ DisjointSet.cpp
@@ -29,3 +29,26 @@ void Union(int i,int j) {
     }
 }
 /* end */
+
+/* Dsu by rank */
+int Rank[1001],Parent[1001];
+
+void Createset(int i) {
+        Parent[i] = i;
+        Rank[i] = 0;
+}
+
+int Findset(int i) {
+        if(i != P[i]) P[i] = Findset(P[i]);
+        return P[i];
+}
+
+void Union(int i,int j) {
+        int Pi = Findset(i);
+        int Pj = Findset(j);
+        if(Rank[Pi] > Rank[Pj]) Parent[Pj] = Pi;
+        else Parent[Pi] = Pj;
+        if(Rank[Pi] == Rank[Pj]) Rank[Pj] += 1;
+}
+/* end */
+
