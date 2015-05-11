664976d61baac9236853a58d4d35534727516dbc
Phong Pham <kajtoukjd1612@users.noreply.github.com>
2015-05-11 06:24:30 -0700

    LCA

--- LCA.cpp
+++ LCA.cpp
@@ -1,66 +1,60 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <vector>
+#include <functional>
+#include <algorithm>
+#include <utility>
+#include <sstream>
+#include <cstdlib>
+#include <bitset>
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
 using namespace std;
- 
-int parent[21][1001];
-int n, root, t, q;
-int Level[1001];
- 
-void Root() {
-    for(int i = 1 ; i <= n ; ++i) {
-        if(parent[0][i] == 0) root = i;
-    }
-    parent[0][root] = root;
+typedef pair< int, int > ii;
+typedef pair< int, ii > iii;
+const int oo = 1e9 + 7;
+
+vector< int > Adj[40010];
+vector< int > color[40010];
+int n, m, q, visit[40010], parent[25][40010], lv[40010];
+
+void dfs(int s) {
+     visit[s] = 1;
+     for(int i = 0 ; i < Adj[s].size() ; ++i) {
+          int u = Adj[s][i];
+          if(!visit[u]) {
+               parent[0][u] = s;
+               lv[u] = lv[s] + 1;
+               dfs(u);
+          }
+     }
 }
- 
-int level(int x) {
-    if(x == root) return 1;
-    if(Level[x]) return Level[x];
-    return Level[x] = level(parent[0][x]) + 1;
-}
- 
+
 int lca(int x, int y) {
-    for(int i = 20 ; i >= 0 ; --i) if(level(parent[i][x]) >= level(y)) {x = parent[i][x];}
-    for(int i = 20 ; i >= 0 ; --i) if(level(parent[i][y]) >= level(x)) {y = parent[i][y];}
-    for(int i = 20 ; i >= 0 ; --i) if(parent[i][x] != parent[i][y]) { x = parent[i][x]; y = parent[i][y]; };
- 
-    while(x != y) {
-        x = parent[0][x];
-        y = parent[0][y];
-    }
- 
-    return x;
+     for(int i = 20 ; i >= 0 ; --i) { if(lv[parent[i][x]] >= lv[y]) x = parent[i][x]; }
+     for(int i = 20 ; i >= 0 ; --i) { if(lv[parent[i][y]] >= lv[x]) y = parent[i][y]; }
+     for(int i = 20 ; i >= 0 ; --i) { if(parent[i][x] != parent[i][y]) { x = parent[i][x]; y = parent[i][y]; } }
+
+     while(x != y) {
+          x = parent[0][x];
+          y = parent[0][y];
+     }
+
+     return x;
 }
- 
-main() {
-    //freopen("LCA.in","r",stdin);
-    scanf("%d", &t);
-    for(int k = 1 ; k <= t ; ++k) {
-        scanf("%d", &n);
-        for(int i = 1 ; i <= n ; ++i) {
-            int u, v;
-            scanf("%d", &u);
-            while(u--) {
-                scanf("%d", &v);
-                parent[0][v] = i;
-            }
-        }
- 
-        //printf("1");
- 
-        Root();
-        for(int i = 1 ; i < 21 ; ++i) {
-            for(int j = 1 ; j <= n ; ++j) {
-                parent[i][j] = parent[i - 1][parent[i - 1][j]];
-            }
-        }
- 
-        //printf("2");
-        scanf("%d", &q);
-        printf("Case %d:\n", k);
-        while(q--) {
-            int u, v;
-            scanf("%d %d", &u, &v);
-            printf("%d\n", lca(u, v));
-        }
-    }
+
+void init() {
+     for(int i = 1 ; i < 21 ; ++i) {
+          for(int j = 1 ; j <= n ; ++j) {
+               parent[i][j] = parent[i - 1][parent[i - 1][j]];
+          }
+     }
 }
