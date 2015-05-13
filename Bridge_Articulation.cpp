d5f6b889fb540ce4d70c29415746b295236ee68b
Phong Pham <kajtoukjd1612@users.noreply.github.com>
2015-05-13 16:26:31 +0700

    Bridge + Articulation

--- Bridge_Articulation.cpp
+++ Bridge_Articulation.cpp
@@ -1,52 +1,79 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <vector>
+#include <functional>
+#include <algorithm>
+#include <utility>
+#include <sstream>
+#include <cstdlib>
+#include <bitset>
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
 using namespace std;
+typedef pair< int, int > ii;
+typedef pair< int, ii > iii;
+const int oo = 1e9 + 7;
 
-vector<long long> Adjlist[100001];
-bool articulation[100000];
-long long num[100001], low[100001], parent[100001], curtime = 0, root, children;
+int n, m, low[100010], num[100010], Time = 0, res = 0;
+vector< int > Adj[100010];
+bool art[100010];
 
-long long a1/*articulation*/, a2/*bridge*/;
-void dfs(int x) {
-    num[x] = low[x] = ++curtime;
-    for(int i = 0 ; i < Adjlist[x].size() ; ++i) {
-        long long cur = Adjlist[x][i];
-        if(!num[cur]) {
-            parent[cur] = x;
-            if(parent[cur] == root) ++children;
-            dfs(cur);
-            if(low[cur] >= num[x]) {
-                articulation[x] = true;
-            }
-            if(low[cur] > num[x]) {
-                ++a2;
-            }
-            low[x] = min(low[cur], low[x]);
-        }
-        else if(cur != parent[x]) {
-            low[x] =min(num[cur], low[x]);
-        }
-    }
+void visit(int u, int p) {
+     int NumChild = 0;
+     low[u] = num[u] = ++Time;
+     for (int i = 0 ; i < Adj[u].size() ; ++i) {
+          int v = Adj[u][i];
+          if(v != p) {
+               if(num[v] != 0) low[u] = min(low[u], num[v]);
+               else {
+                    visit(v, u);
+                    NumChild++;
+                    low[u] = min(low[u], low[v]);
+
+                    if(low[v] >= num[v]) res++;
+
+                    if(u == p) {
+                         if(NumChild >= 2)  art[u] = true;
+                    }
+
+                    else {
+                         if(low[v] >= num[u])
+                         art[u] = true;
+                    }
+               }
+          }
+     }
 }
-
 
 main() {
-    long long n, m, a, b;
-    cin >> n >> m;
-    for(int i = 1 ; i <= m ; ++i) {
-        cin >> a >> b;
-        Adjlist[a].push_back(b);
-        Adjlist[b].push_back(a);
-    }
-    for(int j = 1 ; j <= n ; ++j) {
-        if(!num[j]) {
-            root = j;
-            children = 0;
-            dfs(j);
-            articulation[j]=(children>1);
-        }
-    }
-    for(int i = 1 ; i <= n ; ++i) {
-        if(articulation[i]) ++a1;
-    }
-    cout << a1 << " " << a2;
+     #ifdef Phong_dz
+          freopen("GRAPH_.txt", "r", stdin);
+     #endif
+     scanf("%d %d", &n, &m);
+     for(int i = 0 ; i < m ; ++i) {
+          int u, v;
+          scanf("%d %d", &u, &v);
+          Adj[u].pb(v);
+          Adj[v].pb(u);
+     }
+
+     for(int i = 1 ; i <= n ; ++i) {
+          if(!num[i]) visit(i, i);
+     }
+
+     int res1 = 0;
+     for(int i = 1 ; i <= n ; ++i) {
+          if(art[i]) res1++;
+     }
+
+     printf("%d %d", res1, res);
 }
+
